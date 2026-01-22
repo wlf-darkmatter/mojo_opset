5a172695a79c7394754f6ec51654086592dbcd58
liuyuan.leo <liuyuan.leo@bytedance.com>
2026-01-22 11:54:08 +0800

    test(tests/base/test_tensor_device_guard): add more test cases.

--- tests/base/test_tensor_device_guard.py
+++ tests/base/test_tensor_device_guard.py
@@ -2,10 +2,25 @@ import torch
 import pytest
 import inspect
 from mojo_opset.backends.ttx.kernels.npu.kv_cache import store_paged_kv_impl
+from mojo_opset.backends.ttx.kernels.npu.convolution import causal_conv1d_fwd
+from mojo_opset.backends.ttx.kernels.npu.fused_add_layer_norm import TTXFusedAddLayerNormFunction
 
 def test_tensor_device_guard():
     with pytest.raises(TypeError, match="Found cpu tensor.*triton kernel."):
         store_paged_kv_impl(
             *[torch.empty(*[8]*4)]
             * len(inspect.signature(store_paged_kv_impl).parameters)
+        )
+
+    with pytest.raises(TypeError, match="Found cpu tensor.*triton kernel."):
+        causal_conv1d_fwd(
+            *[torch.empty(*[8]*3)]
+            * 4
+        )
+
+    with pytest.raises(TypeError, match="Found cpu tensor.*triton kernel."):
+        class Dummy:
+            pass
+        TTXFusedAddLayerNormFunction.forward(
+            Dummy(), *[torch.empty(*[8] * 3)] * 4, True, 0.01, False
         )
