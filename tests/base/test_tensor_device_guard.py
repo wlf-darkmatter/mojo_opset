3e8d5ab47a4401fe101804f1de9977b98b5ac661
liuyuan.leo <liuyuan.leo@bytedance.com>
2026-01-26 18:44:26 +0800

    test(tests/base): add unit test for assertion rewrite.

--- tests/base/test_tensor_device_guard.py
+++ tests/base/test_tensor_device_guard.py
@@ -1,6 +1,12 @@
+import os
 import torch
 import pytest
 import inspect
+import importlib
+os.environ["MOJO_DISABLE_TENSOR_GUARD"] = "No"
+# WARNING(liuyuan): The following reload is necessary when running with other tests
+from mojo_opset.backends.ttx.kernels import npu
+importlib.reload(npu)
 from mojo_opset.backends.ttx.kernels.npu.kv_cache import store_paged_kv_impl
 from mojo_opset.backends.ttx.kernels.npu.convolution import causal_conv1d_fwd
 from mojo_opset.backends.ttx.kernels.npu.fused_add_layer_norm import TTXFusedAddLayerNormFunction
@@ -8,8 +14,9 @@ from mojo_opset.backends.ttx.kernels.npu.fused_add_layer_norm import TTXFusedAdd
 def test_tensor_device_guard():
     with pytest.raises(TypeError, match="Found cpu tensor.*triton kernel."):
         store_paged_kv_impl(
-            *[torch.empty(*[8]*4)]
-            * len(inspect.signature(store_paged_kv_impl).parameters)
+            *[torch.empty(*[8]*4)]*5,
+            torch.tensor([2, 4]),
+            torch.tensor([2, 2])
         )
 
     with pytest.raises(TypeError, match="Found cpu tensor.*triton kernel."):
