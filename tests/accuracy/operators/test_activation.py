2adb100f46dfd501bfc66d12eb83c5dbb31a857e
minicybersheep <minicybersheep@outlook.com>
2026-01-28 14:28:44 +0800

    修改测试用例
    (cherry picked from commit 0a8b9e4e5f2e81e3d7ec1d8cbde069d3a07ae905)

--- tests/accuracy/operators/test_activation.py
+++ tests/accuracy/operators/test_activation.py
@@ -1,8 +1,10 @@
 import pytest
 import torch
+import os
 
 from tests.utils import auto_switch_platform
 from tests.utils import bypass_not_implemented
+from mojo_opset.utils.platform import get_platform
 
 from mojo_opset import MojoGelu
 from mojo_opset import MojoSilu
@@ -51,13 +53,37 @@ def test_swiglu(gate_out, up_out):
     swiglu.forward_diff_with(swiglu_ref, gate_out, up_out)
 
 
+dtype_str_map = {
+    "bfloat16": torch.bfloat16,
+    "float32": torch.float32,
+    "float16": torch.float16,
+}
+
 @pytest.mark.parametrize(
-    "x",
-    [(torch.rand(128, 128))],
+    "batch_size, seq_len, n_qh, head_dim, dtype",
+    [
+        (batch_size, 1024, 128, 128, dtype)
+        for batch_size in [2, 8]
+        for dtype in ["bfloat16", "float16", "float32"]
+    ],
 )
 @auto_switch_platform()
 @bypass_not_implemented
-def test_indexer_rotate_activation(x):
-    relu = MojoIndexerRotateActivation()
-    relu_ref = MojoIndexerRotateActivation._registry.get("torch")()
-    relu.forward_diff_with(relu_ref, x)
+def test_indexer_rotate_activation(batch_size, seq_len, n_qh, head_dim, dtype):
+    device = get_platform()
+    map_tol = {
+        "bfloat16": (1.6e-2, 1e-5),
+        "float16": (1e-3, 1e-5),
+        "float32": (1.3e-6, 1e-5),
+    }
+    if device == 'npu':
+        os.environ["CLOSE_MATMUL_K_SHIFT"] = "1"
+    atol, rtol = map_tol[dtype]
+    dtype = dtype_str_map[dtype]
+    
+    # create input tensor
+    x = torch.randn(batch_size, seq_len, n_qh, head_dim, device=device, dtype=dtype)
+
+    res = MojoIndexerRotateActivation()
+    res_ref = MojoIndexerRotateActivation._registry.get("torch")()
+    res.forward_diff_with(res_ref, x, atol=atol, rtol=rtol)
